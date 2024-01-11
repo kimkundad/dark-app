8747c49d7403beffcb24012547291d7a56a83457
kimkundad <kim.kundad@gmail.com>
2024-01-12 00:32:08 +0700

    kim

--- app/Http/Controllers/LeadImportController.php
+++ app/Http/Controllers/LeadImportController.php
@@ -105,7 +105,7 @@ public function cleanData($a) {
 
                 foreach ($data as $sale) {
 
-                    if($sale[0] !== '' || $sale[1] !== ' '){
+                    if(!empty(trim($sale[0]))){
 
                     $sup_pipeline_ch = 0;
 
