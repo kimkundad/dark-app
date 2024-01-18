06412bf86b6da2ba39f25381179eb342626b4986
kimkundad <kim.kundad@gmail.com>
2024-01-18 19:38:45 +0700

    kim

--- resources/views/admin/category_manager/index.blade.php
+++ resources/views/admin/category_manager/index.blade.php
@@ -148,6 +148,7 @@
                                 <!--end::Table body-->
                             </table>
                             <!--end::Table-->
+                            @include('admin.pagination.default', ['paginator' => $objs])
                         </div>
                     </div>
                 </div>
